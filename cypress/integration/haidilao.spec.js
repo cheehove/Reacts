e41941dd4689b6ab869b65402136a582b77631c5
chee hove teng <cheehove@digitalroute.com>
2019-09-10 22:44:07 +0800

    group configurable variable

--- cypress/integration/haidilao.spec.js
+++ cypress/integration/haidilao.spec.js
@@ -1,22 +1,27 @@
 /// <reference types="Cypress" />
 
+const name = 'ch';
+const phone = '0124'; //eg: 0125787879
+const pax = '3';
+const store ='7613' // sunwayPyramid=4929, Pavilion=7613
+
 context('visit', () => {
   describe('Verified visit', () => {
     before(() => {
-      cy.visit('https://lineup.ap.gosnappy.io/lineup/?storeId=4929&force=true&fbclid=IwAR1X6HHeQ5LciK4zBxUg6fY_8fvSX_-uNh3ncImrT1o14Dhx2UoczQN1Rzw');
+      cy.visit(`https://lineup.ap.gosnappy.io/lineup/?storeId=${store}&force=true&fbclid=IwAR1X6HHeQ5LciK4zBxUg6fY_8fvSX_-uNh3ncImrT1o14Dhx2UoczQN1Rzw`);
     });
     it('fill in', () => {
-      Cypress.currentTest.retries(10);
-      cy.visit('https://lineup.ap.gosnappy.io/lineup/?storeId=4929&force=true&fbclid=IwAR1X6HHeQ5LciK4zBxUg6fY_8fvSX_-uNh3ncImrT1o14Dhx2UoczQN1Rzw');
+      Cypress.currentTest.retries(0);
+      cy.visit(`https://lineup.ap.gosnappy.io/lineup/?storeId=${store}&force=true&fbclid=IwAR1X6HHeQ5LciK4zBxUg6fY_8fvSX_-uNh3ncImrT1o14Dhx2UoczQN1Rzw`);
       cy.server();
       cy.route('POST', 'https://lineup.ap.gosnappy.io/v1/la/lineup').as('lineup');
 
       cy.get('#name')
-        .type(''); // insert name
+        .type(name)
       cy.get('#mobile')
-        .type('') // insert phone number
+        .type(phone)
       cy.get('.snappy-picker-selector > .ng-valid')
-        .select('Party of 2')  // change the require pax in number
+        .select(`Party of ${pax}`)
       cy.get('.mat-button-wrapper')
         .click();
       cy.wait('@lineup');
